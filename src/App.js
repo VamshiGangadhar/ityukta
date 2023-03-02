82efb5de9183a6b586a8ef9571ec001146e53775
VamshiGangadhar <80575671+VamshiGangadhar@users.noreply.github.com>
2023-03-02 15:29:25 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ function App() {
         <Route path="/technical" element={<Technical />} />
         <Route path="/nontechnical" element={<Nontechnical />} />
         <Route path="/" element={<Home />} />
+        <Route path="*" element={<Pagenotfound />} />
       </Routes>
     </CssBaseline>
   );
